16b9909972cc9007209928f4ccc4041a9565af51
tariksk355 <tarikyegen35@gmail.com>
2023-10-28 12:34:33 +0000

    Jenkins_project

--- main.tf
+++ main.tf
@@ -6,7 +6,7 @@ terraform {
     }
   }
   backend "s3" {
-    bucket = "jenkins-project-backend-tarkov"
+    bucket = "jenkins-project-backend"
     key = "backend/tf-backend-jenkins.tfstate"
     region = "us-east-1"
   }
@@ -21,14 +21,14 @@ variable "tags" {
 }
 
 variable "user" {
-  default = "tarkov"
+  default = "tarik"
 }
 
 resource "aws_instance" "managed_nodes" {
   ami = "ami-05a5f6298acdb05b6"
   count = 3
   instance_type = "t2.micro"
-  key_name = "tarkovski"  # change with your pem file
+  key_name = "key_name"  # change with your pem file
   vpc_security_group_ids = [aws_security_group.tf-sec-gr.id]
   iam_instance_profile = "jenkins-project-profile-${var.user}" # we created this with jenkins server
   tags = {
@@ -43,9 +43,9 @@ resource "aws_instance" "managed_nodes" {
 }
 
 resource "aws_security_group" "tf-sec-gr" {
-  name = "project208-sec-gr-${var.user}"
+  name = "jenkins-project-sec-gr-${var.user}"
   tags = {
-    Name = "project208-sec-gr"
+    Name = "jenkins-project-sec-gr"
   }
 
   ingress {
