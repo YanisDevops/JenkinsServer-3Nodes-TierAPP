3dad6841866c22b0de10c8b71218efbce24196f4
yanis.devops1@gmail.com <you@example.com>
2024-08-10 20:04:14 +0200

    terraform update

--- main.tf
+++ main.tf
@@ -6,8 +6,8 @@ terraform {
     }
   }
   backend "s3" {
-    bucket = "jenkins-project-backend"
-    key = "backend/tf-backend-jenkins.tfstate"
+    bucket = "jenkins-3tierapp-yb"
+    key    = "backend/tf-backend-jenkins.tfstate"
     region = "us-east-1"
   }
 }
@@ -21,25 +21,54 @@ variable "tags" {
 }
 
 variable "user" {
-  default = "tarik"
+  default = "yanis"
 }
 
-resource "aws_instance" "managed_nodes" {
-  ami = "ami-05a5f6298acdb05b6"
-  count = 3
-  instance_type = "t2.micro"
-  key_name = "key_name" 
-  vpc_security_group_ids = [aws_security_group.tf-sec-gr.id]
-  iam_instance_profile = "jenkins-project-profile-${var.user}" # I created this with jenkins server
-  tags = {
-    Name = "ansible_${element(var.tags, count.index )}"
-    stack = "ansible_project"
-    environment = "development"
-  }
-  user_data = <<-EOF
-            #! /bin/bash
-            dnf update -y
-            EOF
+resource "aws_iam_role" "jenkins_project_role" {
+  name = "jenkins-project-role-${var.user}"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+        Action = "sts:AssumeRole"
+      },
+    ]
+  })
+}
+
+resource "aws_iam_policy" "jenkins_project_policy" {
+  name        = "jenkins-project-policy-${var.user}"
+  description = "Policy for Jenkins project role"
+  policy      = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Action = [
+          "s3:ListBucket",
+          "s3:GetObject",
+          "s3:PutObject",
+          "ec2:DescribeInstances"
+        ]
+        Resource = "*"
+      },
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "jenkins_project_role_policy_attachment" {
+  role       = aws_iam_role.jenkins_project_role.name
+  policy_arn  = aws_iam_policy.jenkins_project_policy.arn
+}
+
+resource "aws_iam_instance_profile" "jenkins_project_instance_profile" {
+  name = "jenkins-project-profile-${var.user}"
+  role = aws_iam_role.jenkins_project_role.name
 }
 
 resource "aws_security_group" "tf-sec-gr" {
@@ -79,6 +108,24 @@ resource "aws_security_group" "tf-sec-gr" {
     to_port     = 0
     cidr_blocks = ["0.0.0.0/0"]
   }
+}
+
+resource "aws_instance" "managed_nodes" {
+  ami                    = "ami-0583d8c7a9c35822c"
+  count                  = 3
+  instance_type          = "t2.micro"
+  key_name               = "Projects_key-1.pem"
+  vpc_security_group_ids = [aws_security_group.tf-sec-gr.id]
+  iam_instance_profile   = aws_iam_instance_profile.jenkins_project_instance_profile.name
+  tags = {
+    Name        = "ansible_${element(var.tags, count.index)}"
+    stack       = "ansible_project"
+    environment = "development"
+  }
+  user_data = <<-EOF
+                #! /bin/bash
+                dnf update -y
+                EOF
 }
 
 output "react_ip" {
